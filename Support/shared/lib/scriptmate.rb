5a95b6b2839a214b0d2ecffe3ecc134a1722985d
Shane Becker <veganstraightedge@gmail.com>
2026-08-31 15:37:12 -0700

    Remove the check for tm_interactive_input.dylib, which nothing builds or ships

--- Support/shared/lib/scriptmate.rb
+++ Support/shared/lib/scriptmate.rb
@@ -25,14 +25,6 @@ def my_popen3(*cmd) # returns [stdin, stdout, strerr, pid]
     STDOUT.reopen(pr[1])
     STDERR.reopen(pe[1])
 
-    tm_interactive_input = "#{ENV['TM_SUPPORT_PATH']}/lib/tm_interactive_input.dylib"
-    if (File.exist? tm_interactive_input)
-      dil = ENV['DYLD_INSERT_LIBRARIES']
-      ENV['DYLD_INSERT_LIBRARIES'] = (dil) ? "#{tm_interactive_input}:#{dil}" : tm_interactive_input unless (dil =~ /#{tm_interactive_input}/)
-      ENV['DYLD_FORCE_FLAT_NAMESPACE'] = "1"
-      ENV['TM_INTERACTIVE_INPUT'] = 'AUTO|ECHO'
-    end
-    
     exec(*cmd)
   }
 
